69a6744ac50f1ae1400193ce051123e78ebca305
Joshua <joewy4me@yahoo.com>
2021-06-11 08:20:08 +0000

    added an index and package.json file

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const handleAllRequests = (requestObject,responseObject) => {
+    console.log('Hi, I just received a request');
+    
+    const url = requestObject.url;
+    if(url ==='/' ){
+        responseObject.write("<h1>Welcome to the home page<h1>")
+    } else if (url ==='/login'){
+        responseObject.write("<h2>Welcome to login page<h2>")
+    }else if (url === '/signup'){
+        responseObject.write("<h2>welcome to signup page<h2>")
+    }else {
+        responseObject.write("<h1>Page not found</h1>")
+    }
+    
+    responseObject.end();
+}
+const server = http.createServer(handleAllRequests); 
+
+server.listen(3000, '127.0.0.1', ()=> console.log('server is ready to accept request'));
